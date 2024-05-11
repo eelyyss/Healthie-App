3bee9fe7477d6a66bd6a37798dabc5322bfc4031
Elys Iorga <iorgaelysgabriela@gmail.com>
2024-05-11 13:07:46 +0200

    Creación de componentes

---
+++ src/app/components/private-tasks/private-tasks.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-private-tasks',
+  templateUrl: './private-tasks.component.html',
+  styleUrl: './private-tasks.component.css'
+})
+export class PrivateTasksComponent {
+
+}
