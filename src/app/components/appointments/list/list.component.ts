1496b572faf69eff2b31385da2b86d9583348f0b
Elys Iorga <iorgaelysgabriela@gmail.com>
2024-06-02 00:07:15 +0200

    Design modifications.

--- src/app/components/appointments/list/list.component.ts
+++ src/app/components/appointments/list/list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { AppointmentService } from '../../../_services/appointment.service';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { AppointmentService } from "../../../_services/appointment.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-list',
@@ -18,11 +18,22 @@ export class ListComponent implements OnInit {
       this.appointmentType = params['type'];
     });
 
+    this.loadAppointments();
+  }
+
+  loadAppointments(): void {
     this.appointments = this.appointmentService.getAppointments();
+  }
+
+  confirmDeleteAppointment(appointment: any): void {
+    const confirmDelete = confirm('¿Está seguro de que desea eliminar la cita?');
+    if (confirmDelete) {
+      this.deleteAppointment(appointment);
+    }
   }
 
   deleteAppointment(appointment: any): void {
     this.appointmentService.deleteAppointment(appointment);
-    this.appointments = this.appointmentService.getAppointments();
+    this.loadAppointments();
   }
 }
